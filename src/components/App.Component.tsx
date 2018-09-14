f5ad944bc3fd78d8ed5d683bf4597319618701b3
Karan Rawal <kr14nov@outlook.com>
2018-09-14 19:31:55 +0530

    Added react, react-dom and fixed some scripts in package.json.

---
+++ src/components/App.Component.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class AppComponent extends React.Component {
+  public render() {
+    return (
+      <div>
+        Hello from app.component.tsx.
+      </div>
+    );
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
-let message: string = "Hello World";
-console.error(message);
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import AppComponent from './components/App.Component';
 
+ReactDOM.render(
+  <AppComponent />,
+  document.getElementById('root'),
+);
